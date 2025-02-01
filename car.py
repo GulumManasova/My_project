952071230b74bbb67b91d496f41d92775c240065
GulumManasova <manasovaguluma@gmail.com>
2025-02-01 19:59:24 +0600

    Initial commit

---
+++ car.py
@@ -0,0 +1,23 @@
+from xml.sax.handler import property_declaration_handler
+
+class Car:
+    def __init__(self, make=None, color=None, model=0, price=0, year=0):
+        self.make = make
+        self.color = color
+        self.model = model
+        self.price = price
+        self.year = year
+
+    def drive(self):
+        print('My brother is driving', self.make)
+
+    def info(self):
+        print('My favorite car is',self.make,'m5',self.model)
+    def info2(self):
+        print('Popular car in our country is :',self.make,self.color,self.model)
+    def show_model(self):
+        print('The most expensive car in the world is :',self.make)
+    def dream(self):
+        print('In the future I wanna buy',self.make,'in',self.color,'color and model is:',self.model)
+    def show_price(self):
+        print(f"The price of this {self.make} {self.model} is: ${self.price}.")
---
+++ main.py
@@ -0,0 +1,26 @@
+from car import Car
+if __name__ == '__main__':
+    car1=Car()
+    car1.make='BMW'
+    print(car1.model)
+    car1.info()
+
+    car1=Car('Mercedes',2002)
+    car1.drive()
+
+    car2=Car('Toyota Camry',' model is ', 70)
+    car2.info2()
+
+    car3=Car('Kia k7')
+    car3.drive()
+
+    car4=Car('Lexus','white ',570)
+    car4.dream()
+
+
+    car6 = Car('Tesla','red','S',price=23500)
+    car6.show_price()
+
+    car7 = Car('Rolls Royce')
+    car7.show_model()
+
